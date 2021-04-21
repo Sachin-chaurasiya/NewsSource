075f4bc13139d63840b255d6cee46929f3222c02
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2021-04-21 14:20:07 +0530

    proptype issue solved

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -3,7 +3,6 @@ import TopStory from "./TopStory";
 import Spinner from "./layouts/Spinner";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
 import { NavLink } from "react-router-dom";
